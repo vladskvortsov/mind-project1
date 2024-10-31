d1d0d049d1cd6ff85c93afcc6f237ed0f12408e7
vladskvortsov <vladskvortsov96@gmail.com>
2024-10-31 12:42:34 +0200

    rds deploy added

--- project1/user_data.sh
+++ project1/user_data.sh
@@ -1,11 +1,11 @@
           #!/bin/bash 
+          cd mind/
           apt update 
           apt install unzip
           curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
           unzip awscliv2.zip
-          ./aws/install          
+          ./aws/install 
+          sudo usermod -aG docker $USER
+          newgrp docker         
           aws ecr get-login-password --region ${{ secrets.AWS_REGION }} | docker login --username AWS --password-stdin ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com
-          docker pull ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-rds
-          docker pull ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-redis
-          cd mind/
           docker compose up -d
