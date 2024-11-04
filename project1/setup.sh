b1bba4899e8eab5476c7bf4c328e76559db0c102
vladskvortsov <vladskvortsov96@gmail.com>
2024-11-04 11:34:54 +0200

    ec2 deploy rebuilded

--- project1/setup.sh
+++ project1/setup.sh
@@ -1,11 +1,11 @@
         #!/bin/bash
         cd mind/
-        apt update
-        apt install -y unzip docker.io
+        apt update -y
+        apt install -y unzip docker
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         unzip awscliv2.zip
         ./aws/install
-        sudo usermod -aG docker $USER
+        sudo usermod -aG docker ubuntu
         newgrp docker
         aws ecr get-login-password --region ${{ secrets.AWS_REGION }} | docker login --username AWS --password-stdin ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com
         docker compose up -d
