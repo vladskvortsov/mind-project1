2ed8ef805cab920fe9466fe96fa6eff65be21436
vladskvortsov <vladskvortsov96@gmail.com>
2024-11-07 14:39:54 +0200

    rebuilded for actions

--- project1/setup.sh
+++ project1/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+AWS_ACCESS_KEY_ID=$1
+
 apt update -y
 apt install -y unzip docker.io docker-compose git
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -7,7 +9,7 @@ unzip awscliv2.zip
 usermod -aG docker ubuntu
 systemctl restart docker
 # newgrp docker
-sudo -u ubuntu aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }}
+sudo -u ubuntu aws configure set aws_access_key_id ${ AWS_ACCESS_KEY_ID }
 sudo -u ubuntu aws configure set aws_secret_access_key  ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 sudo -u ubuntu aws ecr get-login-password --region ${{ secrets.AWS_REGION }} | docker login --username AWS --password-stdin ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com
 cd /home/ubuntu/
