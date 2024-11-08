b30d8b8ab910ed2395c27a165697ad8e31dee4fe
vladskvortsov <vladskvortsov96@gmail.com>
2024-11-08 13:35:44 +0200

    rebuilded for actions

--- project1/setup-test.sh
+++ project1/setup-test.sh
@@ -6,7 +6,7 @@ apt install -y unzip docker.io docker-compose git awscli
 usermod -aG docker ubuntu
 systemctl restart docker
 # newgrp docker
-sudo -u ubuntu aws configure set aws_access_key_id ${ AWS_ACCESS_KEY_ID }
+sudo -u ubuntu aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 sudo -u ubuntu aws configure set aws_secret_access_key  ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 sudo -u ubuntu aws ecr get-login-password --region ${{ secrets.AWS_REGION }} | docker login --username AWS --password-stdin ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com
 cd /home/ubuntu/
