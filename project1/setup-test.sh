6b25d0c034957058837d9e2580c86a9056226877
vladskvortsov <vladskvortsov96@gmail.com>
2024-11-13 16:22:38 +0200

    sm secrets added

--- project1/setup-test.sh
+++ project1/setup-test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-AWS_ACCESS_KEY_ID=$1
-
+echo "$AWS_ACCESS_KEY_ID $AWS_REGION $AWS_ACCOUNT_ID" > vars
 apt update -y
 apt install -y unzip docker.io docker-compose git awscli
 usermod -aG docker ubuntu
 systemctl restart docker
 # newgrp docker
 sudo -u ubuntu aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-sudo -u ubuntu aws configure set aws_secret_access_key  ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-sudo -u ubuntu aws ecr get-login-password --region ${{ secrets.AWS_REGION }} | docker login --username AWS --password-stdin ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com
+sudo -u ubuntu aws configure set aws_secret_access_key  $AWS_SECRET_ACCESS_KEY
+sudo -u ubuntu aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 cd /home/ubuntu/
 git clone https://github.com/vladskvortsov/mind.git
 cd mind/project1/
