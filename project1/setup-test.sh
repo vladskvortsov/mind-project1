32376413d34181bc629890b3493961c0af29607e
vladskvortsov <vladskvortsov96@gmail.com>
2024-11-18 18:51:54 +0200

    sm secrets added

--- project1/setup-test.sh
+++ project1/setup-test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
+env_file: ~/mind/project1/.env
+
 apt update -y
-apt install -y unzip docker.io docker-compose awscli
-usermod -aG docker ubuntu
-systemctl restart docker
+apt install -y awscli
+# usermod -aG docker ubuntu
+# systemctl restart docker
 # newgrp docker
 sudo -u ubuntu aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 sudo -u ubuntu aws configure set aws_secret_access_key  $AWS_SECRET_ACCESS_KEY
 sudo -u ubuntu aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 # cd /home/ubuntu/
 # git clone https://github.com/vladskvortsov/mind.git
-cd mind/project1/
+# cd mind/project1/
 # echo "SECRET_KEY=my-secret-key
 # DEBUG=False
 
@@ -27,32 +30,32 @@ cd mind/project1/
 
 # CORS_ALLOWED_ORIGINS=$CORS_ALLOWED_ORIGINS" > vars.env
 
-echo '
-# version: '3.8'
-services:
-backend_rds:
-    env_file:
-    - vars.env
-    image: $AWS_ACCOUNT_ID.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-rds
-    container_name: backend_rds
-    ports:
-    - "8000:8000"
-    networks:
-    - backend
-    entrypoint: ["sh", "-c", "sleep 10 && python manage.py runserver 0.0.0.0:8000"]
+# echo '
+# # version: '3.8'
+# services:
+# backend_rds:
+#     env_file:
+#     - vars.env
+#     image: $AWS_ACCOUNT_ID.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-rds
+#     container_name: backend_rds
+#     ports:
+#     - "8000:8000"
+#     networks:
+#     - backend
+#     entrypoint: ["sh", "-c", "sleep 10 && python manage.py runserver 0.0.0.0:8000"]
 
-backend_redis:
-    env_file:
-    - vars.env
-    image: ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-redis
-    container_name: backend_redis
-    ports:
-    - "8003:8003"
-    networks:
-    - backend
-    entrypoint: ["sh", "-c", "sleep 10 && python manage.py runserver 0.0.0.0:8003"]
+# backend_redis:
+#     env_file:
+#     - vars.env
+#     image: ${{ secrets.AWS_ACCOUNT_ID }}.dkr.ecr.${{ secrets.AWS_REGION }}.amazonaws.com/project1-backend:backend-redis
+#     container_name: backend_redis
+#     ports:
+#     - "8003:8003"
+#     networks:
+#     - backend
+#     entrypoint: ["sh", "-c", "sleep 10 && python manage.py runserver 0.0.0.0:8003"]
 
-networks:
-backend:
-    driver: bridge' > docker-compose.yml
+# networks:
+# backend:
+#     driver: bridge' > docker-compose.yml
 docker-compose up -d
